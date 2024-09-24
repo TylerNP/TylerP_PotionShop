44532083cb898f5a6d0e22ad7633209ce4df167a
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-09-24 15:21:45 -0700

    0.221 removed () from int

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -8,7 +8,7 @@ from src import database as db
 with db.engine.begin() as connection: 
     result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
     for row in result:
-        if row.num_green_potions() < 10:
+        if row.num_green_potions < 10:
             print("WORKED")
 
 router = APIRouter(
