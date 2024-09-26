b0052b3f4e5dd1c65d2deffef8d6a76d45c1c04e
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-09-26 11:00:45 -0700

    v0.271 fixed sql typo

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -26,7 +26,7 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
         greenMlUsed += 100*potion.quantity
         print(f"potions delievered: {potions_delivered} order_id: {order_id}")
     with db.engine.begin() as connection:
-        greenPotCurr = connection.execute(sqlalchemy.text("SLECT num_green_potions FROM global_inventory")).scalar()
+        greenPotCurr = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
         greenMlCurr = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar()
         connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_potions = {greenPotCurr+greenPotCnt}"))
         connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml = {greenMlCurr-greenMlUsed}"))
