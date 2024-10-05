9c731ffa3410cd63f5e35ca0fab216e6edf7c548
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-10-04 17:05:29 -0700

    pause before bulk insert

--- src/api/carts.py
+++ src/api/carts.py
@@ -84,7 +84,7 @@ def post_visits(visit_id: int, customers: list[Customer]):
     with db.engine.begin() as connection:
         for customer in customers:
             sql_to_execute = "INSERT INTO customers (visit_id, level, customer_name, customer_class) VALUES (%d, %d, '%s', '%s')"
-            connection.execute(sqlalchemy.text(sql_to_execute % (visit_id, customer.level, customer.customer_name, customer.character_class)))
+            #connection.execute(sqlalchemy.text(sql_to_execute % (visit_id, customer.level, customer.customer_name, customer.character_class)))
             visited = True
 
     return [
