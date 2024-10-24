6b53a4e31d7b73635afec6ada3fc03db4653d883
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-10-24 12:33:47 -0700

    almost works, but doesn't take into account cases  when there is no more potions

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -21,6 +21,35 @@ def get_catalog():
                             ORDER BY quantity DESC 
                             LIMIT 6
                         """
+        sql_to_execute = """
+                            SELECT potions.sku, potions.quantity, potions.price, potions.red, potions.green, potions.blue, potions.dark, potions.name
+                            FROM potions, (
+                                SELECT part.sku 
+                                FROM (
+                                    SELECT potions.sku,
+                                        SUM(-1*potion_ledgers.quantity) AS amt 
+                                    FROM potions, potion_ledgers, transactions, ( 
+                                        SELECT time.id AS id
+                                        FROM time
+                                        WHERE time.day = (SELECT time.day FROM time ORDER BY time.id DESC LIMIT 1)
+                                        ORDER BY time.id DESC
+                                        LIMIT 12
+                                        OFFSET 1
+                                    ) AS time_filtered 
+                                    WHERE potions.sku = potion_ledgers.sku 
+                                    AND potion_ledgers.transaction_id = transactions.id 
+                                    AND potion_ledgers.quantity < 0 
+                                    AND transactions.time_id = time_filtered.id
+                                    GROUP BY potions.sku
+                                ) AS part
+                                ORDER BY part.amt DESC
+                            ) AS sold
+                            WHERE potions.sku = sold.sku
+                            AND quantity > 0
+                            ORDER BY quantity DESC
+                            LIMIT 6
+                        """
+        
         potions = connection.execute(sqlalchemy.text(sql_to_execute))
         for potion in potions:
             potions_available.append(
