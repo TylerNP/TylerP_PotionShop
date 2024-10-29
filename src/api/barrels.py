687aaddf46fdf942c814e5d00d13070815cd040b
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-10-29 16:51:14 -0700

    improved barrel plan a little

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -96,6 +96,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     usable_gold = 0
     ml_capacity = 0
+    small_gold = 0
     num_types = 4
     ml_needed = [0]*num_types
     ml_stored = [0]*num_types
@@ -106,6 +107,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                             SELECT 
                                 (10000*ml_capacity) AS capacity, 
                                 gold-(SELECT gold_threshold FROM parameters) AS usable_gold, 
+                                (SELECT starting_gold_saving FROM parameters) AS small_gold,
                                 num_red_ml, 
                                 num_green_ml, 
                                 num_blue_ml, 
@@ -116,6 +118,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         for result in query:
             ml_capacity = result.capacity
             usable_gold = result.usable_gold
+            small_gold = result.small_gold
             ml_stored[0] = result.num_red_ml
             ml_stored[1] = result.num_green_ml
             ml_stored[2] = result.num_blue_ml
@@ -140,59 +143,33 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
             ml_needed[2] += pots.blue*(pots.threshold-pots.quantity)
             ml_needed[3] += pots.dark*(pots.threshold-pots.quantity)
 
-    return barrel_plan_calculation(wholesale_catalog, ml_needed, ml_stored, usable_gold, ml_capacity)
+    return barrel_plan_calculation(wholesale_catalog, ml_needed, ml_stored, usable_gold, small_gold, ml_capacity)
 
 def barrel_plan_calculation(
                                 wholesale_catalog : list[Barrel], 
                                 ml_needed : list[int], 
                                 ml_stored : list[int],
                                 usable_gold : int,
+                                small_gold : int,
                                 ml_capacity : int
                             ) -> dict[str, any]:
     plan = []
     num_types = 4
-    ml_available = [0]*num_types
-    ml_space = [0]*num_types
-    ml_count = 0
-    overflow_count = 0
-    remaining_ml_threshold = 0
 
-    ml_threshold = ml_capacity//num_types
-    total_ml = 0
-    over_threshold = False
-    for index in range(len(ml_needed)):
-        if ml_stored[index] > ml_threshold:
-            over_threshold = True
-            overflow_count = overflow_count + 1
-        ml_available[index] = ml_stored[index]
-        total_ml += ml_stored[index]
-    
-    if (total_ml>=ml_capacity):
-        return plan
-    
-    if over_threshold:
-        remaining_ml_threshold = (ml_capacity-total_ml)
-    
-
-    for value in ml_needed:
-        if value != 0:
-            ml_count = ml_count + 1
+    ml_count = sum(1 for ml in ml_needed if ml != 0)
     if ml_count == 0:
         ml_count = num_types
+    if usable_gold < small_gold:
+        ml_count = 1
 
     #Filter barrels purchasable with gold split between types possible needed and sort by ml per gold
     desired_barrels = []
-    barrel_ml_per_gold = []
     barrel_types = [[] for _ in range(num_types)]
-    small_gold = 500 #gold for large barrel (red/green) 
-    if usable_gold < small_gold:
-        ml_count = 1
     for barrel in wholesale_catalog:
-        if barrel.price > usable_gold//ml_count:
+        if barrel.price > usable_gold//ml_count or barrel.ml_per_barrel > ml_capacity//num_types:
             continue
         desired_barrels.append(barrel)
-        barrel_ml_per_gold.append(barrel.ml_per_barrel//barrel.price)
-    sorted_barrels = [barrel for _, barrel in sorted(zip(barrel_ml_per_gold, desired_barrels), key = lambda pair:pair[0], reverse=True)]
+    sorted_barrels = sorted(desired_barrels, key=lambda x: x.ml_per_barrel//x.price, reverse=True)
 
     #Sort Barrels By Type [R,G,B,D]
     barrel_type = 1
@@ -200,34 +177,39 @@ def barrel_plan_calculation(
         barrel_types[barrel.potion_type.index(barrel_type)].append(barrel)
 
     #determine how much space is available
+    ml_threshold = ml_capacity//num_types
+    not_buying_count = 0
+    overflow_ml = 0
     ml_can_buy = [0]*num_types
-    not_buyable_count = 0
     for index in range(num_types):
-        if len(barrel_types[index]) > 0:
+        if len(barrel_types[index]) > 0 and ml_stored[index] <= ml_threshold:
             ml_can_buy[index] = 1
         else:
-            not_buyable_count = not_buyable_count + 1
-    ml_max = ml_threshold
-    if overflow_count > 0:
+            not_buying_count += 1
+            overflow_ml += ml_stored[index]
+
+    ml_space = [0]*num_types
+    if not_buying_count  > 0:
         #Trys to Refill the low values as fast as possible
-        if (num_types-overflow_count-not_buyable_count) == 0:
+        if num_types == not_buying_count:
             return plan
-        ml_max = remaining_ml_threshold // (num_types-overflow_count-not_buyable_count)
+        ml_threshold = (ml_capacity-overflow_ml) // (num_types-not_buying_count)
     for index in range(num_types):
-        ml_space_remain = ml_max - ml_available[index]
+        ml_space_remain = ml_threshold-ml_stored[index]
         if ml_space_remain > 0:
             ml_space[index] = ml_space_remain  
         else:
             ml_can_buy[index] = 0
-
     # Takes Dotproduct of ml_needed and ml_space and normalize to create weights for buying
-    normal = 0
     make_int = 10
-    for i in range(len(ml_needed)):
-        normal += ml_needed[i]*ml_space[i]
-    if normal == 0:
-        normal = 1
-    ml_ratio = [0]*4
+    normal = sum(x*y for x, y in zip(ml_needed, ml_space))
+    min = 0
+    for i in range(num_types):
+        if ml_space[i] > 0 and ml_space[i] < min or min == 0:
+            min = ml_space[i]
+    normal = normal if normal != 0 else min*make_int
+    ml_ratio = [0]*num_types
+
     for i in range(len(ml_needed)):
         if ml_can_buy[i] == 0:
             ml_ratio[i] = 0
@@ -246,27 +228,10 @@ def barrel_plan_calculation(
 
     #Determine If More ml Can Be Purchased For Later use
     count = 0
-    while True:
+    while any (buy != 0 for buy in ml_can_buy):
         count += 1
-        if not any(ml_can_buy):
+        if all(ml == 0 for ml in ml_ratio_copy):
             break
-        cycle_complete = True
-        for ml in ml_ratio_copy:
-            if ml > 0:
-                cycle_complete = False
-                break
-        #REMOVE ONCE FIXED
-        #==========================#
-        no_more = 0
-        if cycle_complete:
-            for i in range(num_types):
-                ml_ratio_copy[i] = ml_ratio_copy[i] + ml_ratio[i]
-                if ml_ratio_copy[i] == 0:
-                    no_more += 1
-            type_index = max_index
-        if no_more == num_types:
-            break
-        #==========================#
         if ml_ratio_copy[type_index] <= 0 or ml_can_buy[type_index] == 0:
             type_index = (type_index+1) % num_types
             continue
@@ -280,77 +245,58 @@ def barrel_plan_calculation(
             list_of_index[type_index] = list_of_index[type_index] + 1
             continue   
         buy_amt = 1
-        usable_gold = usable_gold-barrel_to_buy.price
-        ml_space[type_index] = ml_space[type_index] - barrel_to_buy.ml_per_barrel
+        usable_gold -= barrel_to_buy.price
+        ml_space[type_index] -= barrel_to_buy.ml_per_barrel
         if barrel_to_buy not in unique_barrels:
             unique_barrels.append(barrel_to_buy)
             buy_count.append(buy_amt)
         else:
             new_quantity = buy_count[unique_barrels.index(barrel_to_buy)] + buy_amt
             if new_quantity > barrel_to_buy.quantity:
-                list_of_index[type_index] = list_of_index[type_index] + 1
+                list_of_index[type_index] += 1
                 continue
             buy_count[unique_barrels.index(barrel_to_buy)] = new_quantity
         relative_change = round(barrel_to_buy.ml_per_barrel*make_int/normal)
         if relative_change < buy_amt:
             relative_change = buy_amt
-        ml_ratio_copy[type_index] = ml_ratio_copy[type_index] - relative_change
+        ml_ratio_copy[type_index] -= relative_change
         type_index = (type_index+1) % num_types 
+        if all(ml <= 0 for ml in ml_ratio_copy):
+            ml_ratio_copy = [ml_ratio[i]+ml_ratio_copy[i] for i in range(num_types)]
+            type_index = max_index
+        
     
     print(f"Looped {count} times")
     for i in range(len(unique_barrels)):
         plan.append( {"sku":unique_barrels[i].sku, "quantity": buy_count[i]} )
     print(plan)
-    return plan
+    return plan  
 
 def simplified_plan(
                         wholesale_catalog : list[Barrel], 
                         ml_needed : list[int], 
                         ml_stored : list[int],
                         usable_gold : int,
+                        small_gold : int,
                         ml_capacity : int
                     ) -> dict[str, any]:
     plan = []
     num_types = 4
-    ml_count = 0
-    overflow_count = 0
-    remaining_ml_threshold = 0
-    ml_available = [0]*num_types
-    ml_space = [0]*num_types
 
-    ml_threshold = ml_capacity//num_types
-    total_ml = 0
-    for index in range(len(ml_needed)):
-        if ml_stored[index] > ml_threshold:
-            overflow_count = overflow_count + 1
-        ml_available[index] = ml_stored[index]
-        total_ml += ml_stored[index]
-    
-    if (total_ml>=ml_capacity):
-        return plan
-    
-    if overflow_count > 0:
-        remaining_ml_threshold = (ml_capacity-total_ml)
-
-    for value in ml_needed:
-        if value != 0:
-            ml_count = ml_count + 1
+    ml_count = sum(1 for ml in ml_needed if ml != 0)
     if ml_count == 0:
         ml_count = num_types
+    if usable_gold < small_gold:
+        ml_count = 1
 
     #Filter barrels purchasable with gold split between types possible needed and sort by ml per gold
     desired_barrels = []
-    barrel_ml_per_gold = []
     barrel_types = [[] for _ in range(num_types)]
-    small_gold = 500 #gold for large barrel (red/green) 
-    if usable_gold < small_gold:
-        ml_count = 1
     for barrel in wholesale_catalog:
-        if barrel.price > usable_gold//ml_count:
+        if barrel.price > usable_gold//ml_count or barrel.ml_per_barrel > ml_capacity//num_types:
             continue
         desired_barrels.append(barrel)
-        barrel_ml_per_gold.append(barrel.ml_per_barrel//barrel.price)
-    sorted_barrels = [barrel for _, barrel in sorted(zip(barrel_ml_per_gold, desired_barrels), key = lambda pair:pair[0], reverse=True)]
+    sorted_barrels = sorted(desired_barrels, key=lambda x: x.ml_per_barrel//x.price, reverse=True)
 
     #Sort Barrels By Type [R,G,B,D]
     barrel_type = 1
@@ -358,34 +304,39 @@ def simplified_plan(
         barrel_types[barrel.potion_type.index(barrel_type)].append(barrel)
 
     #determine how much space is available
+    ml_threshold = ml_capacity//num_types
+    not_buying_count = 0
+    overflow_ml = 0
     ml_can_buy = [0]*num_types
-    not_buyable_count = 0
     for index in range(num_types):
-        if len(barrel_types[index]) > 0:
+        if len(barrel_types[index]) > 0 and ml_stored[index] <= ml_threshold:
             ml_can_buy[index] = 1
         else:
-            not_buyable_count = not_buyable_count + 1
-    ml_max = ml_threshold
-    if overflow_count > 0:
+            not_buying_count += 1
+            overflow_ml += ml_stored[index]
+
+    ml_space = [0]*num_types
+    if not_buying_count  > 0:
         #Trys to Refill the low values as fast as possible
-        if (num_types-overflow_count-not_buyable_count) == 0:
+        if num_types == not_buying_count:
             return plan
-        ml_max = remaining_ml_threshold // (num_types-overflow_count-not_buyable_count)
+        ml_threshold = (ml_capacity-overflow_ml) // (num_types-not_buying_count)
     for index in range(num_types):
-        ml_space_remain = ml_max - ml_available[index]
+        ml_space_remain = ml_threshold-ml_stored[index]
         if ml_space_remain > 0:
             ml_space[index] = ml_space_remain  
         else:
             ml_can_buy[index] = 0
-
     # Takes Dotproduct of ml_needed and ml_space and normalize to create weights for buying
-    normal = 0
     make_int = 10
-    for i in range(len(ml_needed)):
-        normal += ml_needed[i]*ml_space[i]
-    if normal == 0:
-        normal = 1
-    ml_ratio = [0]*4
+    normal = sum(x*y for x, y in zip(ml_needed, ml_space))
+    min = 0
+    for i in range(num_types):
+        if ml_space[i] > 0 and ml_space[i] < min or min == 0:
+            min = ml_space[i]
+    normal = normal if normal != 0 else min*make_int
+    ml_ratio = [0]*num_types
+
     for i in range(len(ml_needed)):
         if ml_can_buy[i] == 0:
             ml_ratio[i] = 0
@@ -404,27 +355,10 @@ def simplified_plan(
 
     #Determine If More ml Can Be Purchased For Later use
     count = 0
-    while True:
+    while any (buy != 0 for buy in ml_can_buy):
         count += 1
-        if not any(ml_can_buy):
+        if all(ml == 0 for ml in ml_ratio_copy):
             break
-        cycle_complete = True
-        for ml in ml_ratio_copy:
-            if ml > 0:
-                cycle_complete = False
-                break
-        #REMOVE ONCE FIXED
-        #==========================#
-        no_more = 0
-        if cycle_complete:
-            for i in range(num_types):
-                ml_ratio_copy[i] = ml_ratio_copy[i] + ml_ratio[i]
-                if ml_ratio_copy[i] == 0:
-                    no_more += 1
-            type_index = max_index
-        if no_more == num_types:
-            break
-        #==========================#
         if ml_ratio_copy[type_index] <= 0 or ml_can_buy[type_index] == 0:
             type_index = (type_index+1) % num_types
             continue
@@ -438,34 +372,38 @@ def simplified_plan(
             list_of_index[type_index] = list_of_index[type_index] + 1
             continue   
         buy_amt = 1
-        usable_gold = usable_gold-barrel_to_buy.price
-        ml_space[type_index] = ml_space[type_index] - barrel_to_buy.ml_per_barrel
+        usable_gold -= barrel_to_buy.price
+        ml_space[type_index] -= barrel_to_buy.ml_per_barrel
         if barrel_to_buy not in unique_barrels:
             unique_barrels.append(barrel_to_buy)
             buy_count.append(buy_amt)
         else:
             new_quantity = buy_count[unique_barrels.index(barrel_to_buy)] + buy_amt
             if new_quantity > barrel_to_buy.quantity:
-                list_of_index[type_index] = list_of_index[type_index] + 1
+                list_of_index[type_index] += 1
                 continue
             buy_count[unique_barrels.index(barrel_to_buy)] = new_quantity
         relative_change = round(barrel_to_buy.ml_per_barrel*make_int/normal)
         if relative_change < buy_amt:
             relative_change = buy_amt
-        ml_ratio_copy[type_index] = ml_ratio_copy[type_index] - relative_change
+        ml_ratio_copy[type_index] -= relative_change
         type_index = (type_index+1) % num_types 
+        if all(ml <= 0 for ml in ml_ratio_copy):
+            ml_ratio_copy = [ml_ratio[i]+ml_ratio_copy[i] for i in range(num_types)]
+            type_index = max_index
+        
     
     print(f"Looped {count} times")
+    total_color = [0]*4
     for i in range(len(unique_barrels)):
         plan.append( {"sku":unique_barrels[i].sku, "quantity": buy_count[i]} )
+        total_color[unique_barrels[i].potion_type.index(1)] += unique_barrels[i].ml_per_barrel * buy_count[i]
+    print(total_color)
     print(plan)
     return plan    
 
 if __name__ == "__main__":
     print("Ran barrels.py")
-    """
-    Test Barrel Plan Logic
-    """
     barrel_catalog = [
                         Barrel(sku='MEDIUM_RED_BARREL', ml_per_barrel=2500, potion_type=[1, 0, 0, 0], price=250, quantity=10), 
                         Barrel(sku='SMALL_RED_BARREL', ml_per_barrel=500, potion_type=[1, 0, 0, 0], price=100, quantity=10), 
@@ -479,14 +417,24 @@ if __name__ == "__main__":
                         Barrel(sku='LARGE_DARK_BARREL', ml_per_barrel=10000, potion_type=[0, 0, 0, 1], price=750, quantity=10), 
                         Barrel(sku='LARGE_BLUE_BARREL', ml_per_barrel=10000, potion_type=[0, 0, 1, 0], price=600, quantity=30), 
                         Barrel(sku='LARGE_GREEN_BARREL', ml_per_barrel=10000, potion_type=[0, 1, 0, 0], price=400, quantity=30), 
-                        Barrel(sku='LARGE_RED_BARREL', ml_per_barrel=10000, potion_type=[1, 0, 0, 0], price=500, quantity=30)
+                        Barrel(sku='LARGE_RED_BARREL', ml_per_barrel=10000, potion_type=[1, 0, 0, 0], price=500, quantity=30),
+                         Barrel(sku='LARGE_DARK_BARREL', ml_per_barrel=10000, potion_type=[0, 0, 0, 1], price=750, quantity=30)
                     ]
+    ml_needed = [0,0,0,0]
+    ml_stored = [3500,500,500,0]
+    usable_gold = 5500
+    small_gold = 500
+    ml_capacity = 10000
+
     ml_needed = [5000,1000,500,0]
-    ml_stored = [5100,500,500,0]
-    usable_gold = 500
-    ml_capacity = 20000
-    simplified_plan(barrel_catalog, ml_needed, ml_stored, usable_gold, ml_capacity)
-    print("\nSEPERATOR\n")
-    barrel_plan_calculation(barrel_catalog, ml_needed, ml_stored, usable_gold, ml_capacity)
+    ml_stored = [1000,500,500,0]
+    usable_gold = 2500
+    small_gold = 500
+    ml_capacity = 40000
+
+    simplified_plan(barrel_catalog, ml_needed, ml_stored, usable_gold, small_gold, ml_capacity)
+    #barrel_plan_calculation(barrel_catalog, ml_needed, ml_stored, usable_gold, small_gold, ml_capacity)
+    #get_wholesale_purchase_plan(barrel_catalog)
+    
     
 
---
+++ test/test.py
@@ -0,0 +1,100 @@
+from src.api import barrels
+from src.api.barrels import Barrel
+import unittest
+
+
+"""
+Test Barrel Plan Logic
+"""
+class TestBarrelFunctions(unittest.TestCase):
+    barrel_catalog = [
+                        Barrel(sku='MEDIUM_RED_BARREL', ml_per_barrel=2500, potion_type=[1, 0, 0, 0], price=250, quantity=10), 
+                        Barrel(sku='SMALL_RED_BARREL', ml_per_barrel=500, potion_type=[1, 0, 0, 0], price=100, quantity=10), 
+                        Barrel(sku='MEDIUM_GREEN_BARREL', ml_per_barrel=2500, potion_type=[0, 1, 0, 0], price=250, quantity=10), 
+                        Barrel(sku='SMALL_GREEN_BARREL', ml_per_barrel=500, potion_type=[0, 1, 0, 0], price=100, quantity=10), 
+                        Barrel(sku='MEDIUM_BLUE_BARREL', ml_per_barrel=2500, potion_type=[0, 0, 1, 0], price=300, quantity=10),
+                        Barrel(sku='SMALL_BLUE_BARREL', ml_per_barrel=500, potion_type=[0, 0, 1, 0], price=120, quantity=10), 
+                        Barrel(sku='MINI_RED_BARREL', ml_per_barrel=200, potion_type=[1, 0, 0, 0], price=60, quantity=1), 
+                        Barrel(sku='MINI_GREEN_BARREL', ml_per_barrel=200, potion_type=[0, 1, 0, 0], price=60, quantity=1), 
+                        Barrel(sku='MINI_BLUE_BARREL', ml_per_barrel=200, potion_type=[0, 0, 1, 0], price=60, quantity=1), 
+                        Barrel(sku='LARGE_DARK_BARREL', ml_per_barrel=10000, potion_type=[0, 0, 0, 1], price=750, quantity=10), 
+                        Barrel(sku='LARGE_BLUE_BARREL', ml_per_barrel=10000, potion_type=[0, 0, 1, 0], price=600, quantity=30), 
+                        Barrel(sku='LARGE_GREEN_BARREL', ml_per_barrel=10000, potion_type=[0, 1, 0, 0], price=400, quantity=30), 
+                        Barrel(sku='LARGE_RED_BARREL', ml_per_barrel=10000, potion_type=[1, 0, 0, 0], price=500, quantity=30),
+                         Barrel(sku='LARGE_DARK_BARREL', ml_per_barrel=10000, potion_type=[0, 0, 0, 1], price=750, quantity=30)
+                    ]
+    def test_plan_simplified_1(self):
+        expected = [
+                        {'sku': 'MEDIUM_RED_BARREL', 'quantity': 3}, 
+                        {'sku': 'MEDIUM_GREEN_BARREL', 'quantity': 2}, 
+                        {'sku': 'MEDIUM_BLUE_BARREL', 'quantity': 1}, 
+                        {'sku': 'LARGE_DARK_BARREL', 'quantity': 1}, 
+                        {'sku': 'SMALL_RED_BARREL', 'quantity': 2}
+                    ]
+        ml_needed = [5000,1000,500,0]
+        ml_stored = [1000,500,500,0]
+        usable_gold = 2500
+        small_gold = 500
+        ml_capacity = 40000
+        self.assertEqual(barrels.simplified_plan(TestBarrelFunctions.barrel_catalog, ml_needed, ml_stored, usable_gold, small_gold, ml_capacity), expected)
+    def test_plan_simplified_2(self):
+        expected = [
+                        {'sku': 'SMALL_RED_BARREL', 'quantity': 2}, 
+                        {'sku': 'SMALL_GREEN_BARREL', 'quantity': 1}, 
+                        {'sku': 'SMALL_BLUE_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_GREEN_BARREL', 'quantity': 1}
+                    ]
+        ml_needed = [1000,1000,500,0]
+        ml_stored = [100,500,500,0]
+        usable_gold = 500
+        small_gold = 500
+        ml_capacity = 10000
+        
+        self.assertEqual(barrels.simplified_plan(TestBarrelFunctions.barrel_catalog, ml_needed, ml_stored, usable_gold, small_gold, ml_capacity), expected)
+    def test_plan_simplified_3(self):
+        expected = [
+                        {'sku': 'SMALL_RED_BARREL', 'quantity': 1}, 
+                        {'sku': 'SMALL_GREEN_BARREL', 'quantity': 1}, 
+                        {'sku': 'SMALL_BLUE_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_RED_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_GREEN_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_BLUE_BARREL', 'quantity': 1}
+                    ]
+        ml_needed = [1000,1000,500,0]
+        ml_stored = [2500,2500,2500,0]
+        usable_gold = 500
+        small_gold = 500
+        ml_capacity = 10000
+        self.assertEqual(barrels.simplified_plan(TestBarrelFunctions.barrel_catalog, ml_needed, ml_stored, usable_gold, small_gold, ml_capacity), expected)
+    def test_plan_simplified_4(self):
+        expected = [
+                        {'sku': 'MEDIUM_RED_BARREL', 'quantity': 1}, 
+                        {'sku': 'MEDIUM_GREEN_BARREL', 'quantity': 1}, 
+                        {'sku': 'MEDIUM_BLUE_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_RED_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_GREEN_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_BLUE_BARREL', 'quantity': 1}
+                    ]
+        ml_needed = [0,0,0,0]
+        ml_stored = [500,500,500,0]
+        usable_gold = 1500
+        small_gold = 500
+        ml_capacity = 10000
+        self.assertEqual(barrels.simplified_plan(TestBarrelFunctions.barrel_catalog, ml_needed, ml_stored, usable_gold, small_gold, ml_capacity), expected)
+    def test_plan_simplified_5(self):
+        expected = [
+                        {'sku': 'MEDIUM_GREEN_BARREL', 'quantity': 1}, 
+                        {'sku': 'MEDIUM_BLUE_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_GREEN_BARREL', 'quantity': 1}, 
+                        {'sku': 'MINI_BLUE_BARREL', 'quantity': 1}
+                    ]
+        ml_needed = [0,0,0,0]
+        ml_stored = [3500,500,500,0]
+        usable_gold = 1500
+        small_gold = 500
+        ml_capacity = 10000
+        self.assertEqual(barrels.simplified_plan(TestBarrelFunctions.barrel_catalog, ml_needed, ml_stored, usable_gold, small_gold, ml_capacity), expected)
+    
+
+if __name__ == '__main__':
+    unittest.main()
